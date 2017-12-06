5993f934eca5457504a34f0c7a1db5301b6bf249
Stepan Poperechnyi <poperechnyi1@gmail.com>
2017-12-06 12:57:33 +0200

    inheritance from interface

--- contracts/ERC223_receiving_contract.sol
+++ contracts/ERC223_receiving_contract.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.4.11;
  /**
  * @title Contract that will work with ERC223 tokens.
  */
+
+ import "./Receiver_Interface.sol"
  
-contract ERC223ReceivingContract { 
+contract ERC223ReceivingContract is Receiver_Interface { 
 /**
  * @dev Standard ERC223 function that will handle incoming token transfers.
  *
